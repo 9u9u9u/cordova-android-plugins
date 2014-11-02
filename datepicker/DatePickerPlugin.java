d705262597a28cea87c41c64015e40c1dd7b70bc
9u9u9u <b1659184@trbvm.com>
2014-11-02 18:31:37 +0100

    Update DatePickerPlugin.java

--- datepicker/DatePickerPlugin.java
+++ datepicker/DatePickerPlugin.java
@@ -145,13 +145,14 @@ public class DatePickerPlugin extends CordovaPlugin {
                 @Override
                 public void run() {
                     final DateSetListener dateSetListener = new DateSetListener(datePickerPlugin, callbackContext);
-                    final DatePickerDialog dateDialog = new DatePickerDialog(currentCtx, android.R.style.Theme_Holo_DialogWhenLarge, dateSetListener, mYear,
+                    final DatePickerDialog dateDialog = new DatePickerDialog(currentCtx, android.R.style.Theme_Holo_Dialog_MinWidth, dateSetListener, mYear,
                             mMonth, mDay);
                             //Theme_Holo_Light_Dialog
                             //Widget_Holo_DatePicker
-                            //Theme_Holo_Dialog
                             //Theme_Holo
+                            //Theme_Holo_Dialog
                             //Theme_Holo_Dialog_NoActionBar
+                            //Theme_Holo_DialogWhenLarge
                     if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
                         DatePicker dp = dateDialog.getDatePicker();
                         if (minDate > 0) {
